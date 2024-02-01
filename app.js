34fc9f652480de5b57bcc75122c19b6f260b2413
itzkushagra <info.kushagrapandey@gmail.com>
2024-02-01 23:03:12 +0530

    update 2

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const elem = <span>React Element</span>;
 // React Element
-const heading = React.createElement(
-    "hi",
-    {id:"heading"}, 
-    "Hello World"
- );
+
+const title = (
+  <h1 className="head" tabIndex="5">
+    {elem}
+    Namaste React using JSX
+  </h1>
+);
+
+const data = 1000;
+
+const HeadingComp = ()=>(
+  <div id= "container">
+    {title} 
+    <h1 className="heading">Bas badhiya bhai</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComp />);     

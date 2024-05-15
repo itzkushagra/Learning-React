81677f8679bcae39a9be5bb75cb9c28e00dd7e83
itzkushagra <info.kushagrapandey@gmail.com>
2024-05-15 22:06:28 +0530

    added app router for about

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client"; 
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import About from "./components/about.js";
 
 const AppLayout = () => {
   return(
@@ -12,6 +14,17 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <AppLayout />,
+  },
+  {
+    path:"/about",
+    element: <About />,
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);     
+root.render(<RouterProvider router={appRouter} />);     
---
+++ src/components/about.js
@@ -0,0 +1,14 @@
+const About = () => {
+    return(
+        <div>
+            <h1>
+                About
+            </h1>
+            <h3>
+                This is the about us section of Namaste React
+            </h3>
+        </div>
+    )
+}
+
+export default About;

ee92eeb53a0f6fb706e261470a4fdf967bcb3673
itzkushagra <info.kushagrapandey@gmail.com>
2024-05-17 21:50:46 +0530

    Added all of the routing

--- src/app.js
+++ src/app.js
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client"; 
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
 import About from "./components/About.js";
 import Contact from "./components/Contact.js";
+import Error from "./components/Error.js";
 
 const AppLayout = () => {
   return(
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -19,15 +20,23 @@ const appRouter = createBrowserRouter([
   {
     path:"/",
     element: <AppLayout />,
+    children:[
+      {
+        path:"/",
+        element: <Body />,
+      },
+      {
+        path:"/about",
+        element: <About />,
+      },
+      {
+        path:"/contact",
+        element: <Contact />,
+      }
+    ],
+    errorElement: <Error />,
   },
-  {
-    path:"/about",
-    element: <About />,
-  },
-  {
-    path:"/contact",
-    element: <Contact />,
-  }
+ 
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ src/components/Error.js
@@ -0,0 +1,10 @@
+const Error = ()=>{
+    return(
+        <div>
+            <h1>Error</h1>
+            <h3>Opps ! Something went wrong !!!</h3>
+        </div>
+    )
+}
+
+export default Error;

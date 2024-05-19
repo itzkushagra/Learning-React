4dd0ea20b3b857533d488c6cf8e4c188868d5b35
itzkushagra <info.kushagrapandey@gmail.com>
2024-05-19 21:13:16 +0530

    RestMenu added

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const RestaurantMenu = ()=>{
+
+    const {restInfo, setResInfo} = useState(null);
 
     useEffect(()=>{
         fetchMenu();
@@ -10,9 +13,11 @@ const RestaurantMenu = ()=>{
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.95250&lng=75.71050&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
         console.log(json);
+        setResInfo(json.data);
     }
 
-    return(
+    return restInfo==null ?<Shimmer />:(
+
         <div className="menu">
             <h1>Name of Rest</h1>
             <h2>Menu</h2>
